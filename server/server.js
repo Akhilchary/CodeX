f5205f1de52e9ff139507b3ec22dc88d73f01dc3
Peddoji Akhil <peddoji_akhil@epam.com>
2023-10-04 11:36:32 +0530

    route file added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 
+const codeRoutes=require('./routes/code.routes');
 
 const app=express();
 const PORT=3000;
@@ -8,7 +9,7 @@ const PORT=3000;
 
 // middleware
 app.use(cors());
-
+app.use("/api/",codeRoutes);
 //    --global error handler
 app.use((err,req,res,next)=>{
     res.status(err.status),
